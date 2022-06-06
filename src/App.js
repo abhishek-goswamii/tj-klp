62060afadd4e64913aa51578f03566b2af9a05e4
abhix18 <abi.xa12@gmail.com>
2022-06-06 12:01:46 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/header/header';
+import Homepage from './components/homepage/homepage';
+
+const App = () => {
+  return (
+      <>
+      <Header/>
+      <Homepage/>
+      </>
+  )
+}
+
+export default App
